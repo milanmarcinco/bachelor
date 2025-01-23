5e9a3dc6fbd71d0394ef9d809e58d289cb1c784b
Milan Marcinčo <mmsprava@gmail.com>
2025-01-23 14:50:29 +0100

    Clean up, use 4 worker processes

--- src/text_service/main.py
+++ src/text_service/main.py
@@ -4,40 +4,6 @@ from multiprocessing import Process, Queue
 from text_handler.TextService import TextHandler
 
 
-def perform_dummy(idx: int, docs: Queue):
-    log_file = open(f"output/logs_{idx}.txt", "w")
-
-    while True:
-        try:
-            doc = docs.get_nowait()
-        except:
-            break
-
-        id = doc["pk"]
-        log_file.write(f"[{id}]: Starting...\n")
-
-        sentences = ["sentence1", "sentence2", "sentence3"]
-        paragraphs = ["paragraph1", "paragraph2", "paragraph3"]
-        pages = ["page1", "page2", "page3"]
-
-        with open(f"output/{id}.json", "w") as file:
-            output = {
-                "sentences": sentences,
-                "paragraphs": paragraphs,
-                "pages": pages
-            }
-
-            json.dump(
-                output,
-                file,
-                indent=2,
-                ensure_ascii=False
-            )
-
-        a, b, c = len(sentences), len(paragraphs), len(pages)
-        log_file.write(f"[{id}]: {a} {b} {c}\n")
-
-
 def perform(idx: int, docs: Queue):
     log_file = open(f"output/logs_{idx}.txt", "w")
 
@@ -103,7 +69,7 @@ if __name__ == "__main__":
             args=(idx, docs),
             daemon=True
         )
-        for idx in range(1)
+        for idx in range(4)
     ]
 
     for process in processes:
